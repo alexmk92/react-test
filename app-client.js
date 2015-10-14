8fa295ae33a2b6df970db30692c00c75973b41b9
Alex Sims <alexander.sims92@gmail.com>
2015-10-14 20:21:26 +0100

    First push

---
+++ app-client.js
@@ -0,0 +1,5 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var demo = require('./components/demo')
+
+ReactDOM.render(<demo />, document.getElementById('react-container'))
---
+++ components/demo.js
@@ -0,0 +1,11 @@
+var React = require('react')
+
+var demo = React.createClass({
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+})
+
+module.exports = demo
