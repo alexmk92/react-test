061dbca6c967be7ad2014e64b695080ca0333c74
Alex Sims <alexander.sims92@gmail.com>
2015-10-15 11:05:36 +0100

    Added socket.io

--- app-server.js
+++ app-server.js
@@ -4,5 +4,11 @@ var app = express()
 app.use(express.static('./public'))
 app.use(express.static('./node_modules/bootstrap/dist'))
 
-app.listen(3000)
+var server = app.listen(3000)
 console.log('polling server running on localhost:3000')
+
+// Create a socket server to listen to inbound connections
+var io = require('socket.io').listen(server)
+io.sockets.on('connection', function(socket) {
+    console.log("Connected: %s", socket.id)
+})
--- components/TestNode.js
+++ components/TestNode.js
@@ -1,6 +1,14 @@
 var React = require('react')
+var io = require('socket.io-client')
 
 var TestNode = React.createClass({
+    componentWillMount() {
+        this.socket = io('http://localhost:3000')
+        this.socket.on('connect', this.connect)
+    },
+    connect() {
+        alert("Connected" + this.socket.id)
+    },
     render() {
         return (
             <h1>Hello world from React</h1>
